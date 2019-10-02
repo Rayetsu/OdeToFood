0fa955b49a792afc7a4cd9f5f4fb8ceda1ffba37
Rayetsu <gabriel25asilva@gmail.com>
2019-10-02 10:22:57 +0100

    Local Db module.

--- OdeToFood.Web/App_Start/ContainerConfig.cs
+++ OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -19,9 +19,16 @@ namespace OdeToFood.Web
 
             builder.RegisterControllers(typeof(MvcApplication).Assembly);
             builder.RegisterApiControllers(typeof(MvcApplication).Assembly);
-            builder.RegisterType<InMemoryRestaurantData>()
+
+            //InMemoryData
+            //builder.RegisterType<InMemoryRestaurantData>()
+            //    .As<IRestaurantData>()
+            //    .SingleInstance();
+
+            builder.RegisterType<SqlRestaurantData>()
                 .As<IRestaurantData>()
-                .SingleInstance();
+                .InstancePerRequest();
+            builder.RegisterType<OdeToFoodDbContext>().InstancePerRequest();
 
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -74,13 +74,31 @@ namespace OdeToFood.Web.Controllers
         [HttpPost]
         public ActionResult Edit(Restaurant restaurant)
         {
-
             if (ModelState.IsValid)
             {
                 db.Update(restaurant);
                 return RedirectToAction("Details", new { id = restaurant.Id });
             }
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var model = db.Get(id);
+            if (model == null)
+            {
+                return View("NotFound");
+            }
+            return View(model);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection form)
+        {
+            db.Delete(id);
+            return RedirectToAction("Index");
         }
     }
 }
